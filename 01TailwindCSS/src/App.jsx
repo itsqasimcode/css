deef0856437714902e6e7585b4f13fb3ebc04f0a
Muhammad Qasim <qasim.alijazz@gmail.com>
2026-05-08 17:31:01 +0500

    installations

---
+++ 01TailwindCSS/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Index from './components/index'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Index/>
+       
+    </>
+  )
+}
+
+export default App
---
+++ 01TailwindCSS/src/components/index.jsx
@@ -0,0 +1,12 @@
+ function Index() {
+
+
+  
+  return (
+    <h1 className="text-5xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Index
