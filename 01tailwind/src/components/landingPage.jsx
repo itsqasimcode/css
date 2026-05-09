be4f93a82da4109f8b3be692a62bae3bb6f34a57
Muhammad Qasim <qasim.alijazz@gmail.com>
2026-05-09 22:10:45 +0500

    Tailwind basics

---
+++ 01tailwind/src/components/landingPage.jsx
@@ -0,0 +1,27 @@
+
+
+ function landingPage() {
+  return (
+    <>
+    <head>
+      <title>Best Used Phones in Lahore - Grap the Deal Now</title>
+    </head>
+    <body>
+       <nav className="bg-purple-800 text-white flex justify-between ">
+        <img src="https://randomuser.me/api/portraits/men/75.jpg" alt="image " className="w-4"/>
+
+        <ul className="px-28 py-4 flex space-x-9 justify-end">
+          <li>Home</li>
+          <li>Pages</li>
+          <li>Sections</li>
+          <li>Doc</li>
+        </ul>
+       </nav>
+    </body>
+    </>
+  )
+}
+
+export default landingPage;
+          
+    
