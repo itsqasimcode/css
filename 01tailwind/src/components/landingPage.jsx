80b0e1b88cd37010837d894c93962360a460ad85
Muhammad Qasim <qasim.alijazz@gmail.com>
2026-05-11 22:12:40 +0500

    Tailwind Fonts and Sizes

--- 01tailwind/src/components/landingPage.jsx
+++ 01tailwind/src/components/landingPage.jsx
@@ -1,27 +1,66 @@
+import bgImage from "../assets/bg.png.png";
 
-
- function landingPage() {
+function LandingPage() {
   return (
     <>
-    <head>
-      <title>Best Used Phones in Lahore - Grap the Deal Now</title>
-    </head>
-    <body>
-       <nav className="bg-purple-800 text-white flex justify-between ">
-        <img src="https://randomuser.me/api/portraits/men/75.jpg" alt="image " className="w-4"/>
+      <head>
+        <title>Best Used Phones in Lahore - Grab the Deal Now</title>
+      </head>
 
-        <ul className="px-28 py-4 flex space-x-9 justify-end">
-          <li>Home</li>
-          <li>Pages</li>
-          <li>Sections</li>
-          <li>Doc</li>
-        </ul>
-       </nav>
-    </body>
+      <body>
+        {/* Navbar */}
+        <nav className="bg-purple-800 text-white flex justify-between items-center px-4">
+          {/* <img
+            src="https://randomuser.me/api/portraits/men/75.jpg"
+            alt="logo"
+            className="h-12 w-12 rounded-xl py-2"
+          />*/}
+          <span className="text-xl font-bold flex items-center">PhonesMania</span>
+
+          <ul className="flex space-x-9 py-4">
+            <li className="cursor-pointer hover:text-fuchsia-300">Home</li>
+            <li className="cursor-pointer hover:text-fuchsia-300">Pages</li>
+            <li className="cursor-pointer hover:text-fuchsia-300">Sections</li>
+            <li className="cursor-pointer hover:text-fuchsia-300">Docs</li>
+          </ul>
+        </nav>
+
+        {/* Main Section */}
+        <main className="bg-fuchsia-200 flex items-center justify-between px-10 py-20">
+          
+          {/* Left Content */}
+          <div className="w-1/2">
+            <h1 className="text-6xl font-bold">
+              The Best Phones in the Town
+            </h1>
+
+            <p className="py-6 text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Saepe voluptatum quaerat, inventore at veniam qui ea
+              nesciunt nisi.
+            </p>
+
+            <button className="bg-purple-800 px-5 py-2 mr-4 rounded-2xl hover:bg-purple-400 hover:text-black text-white">
+              Click Me
+            </button>
+
+            <button className="bg-purple-800 px-5 py-2 rounded-2xl hover:bg-purple-400 hover:text-black text-white">
+              Contact Us
+            </button>
+          </div>
+
+          {/* Right Image */}
+          <div className="w-1/2 flex justify-center">
+            <img
+              src={bgImage}
+              alt="image"
+              className="h-80 "
+            />
+          </div>
+        </main>
+      </body>
     </>
-  )
+  );
 }
 
-export default landingPage;
-          
-    
+export default LandingPage;
